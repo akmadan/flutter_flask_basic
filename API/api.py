a200348f95a8ab22f2bd2d2eecf294d25ecebd1e
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-07-05 20:01:30 +0530

    last

--- API/api.py
+++ API/api.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 @app.route('/api', methods = ['GET'])
 def hello():
     d={}
-    d['ans'] = str(request.args['Query'])
+    inputchr = str(request.args['character'])
+    d['output'] = str(ord(inputchr))
     return jsonify(d)
 
 if __name__ == '__main__':
     app.run()
+
+
